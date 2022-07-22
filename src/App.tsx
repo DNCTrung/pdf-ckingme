d5428c9dfc904d65ca1ab17be848ba6247e94087
Trung Nguyen <trung.nc@3si.vn>
2022-07-22 15:42:23 +0700

    [feat] initial with pdf-ckingme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Template, BLANK_PDF, generate } from '@pdfme/generator';
+// import { Template, BLANK_PDF } from '@pdfme/ui'; <- Template types and BLANK_PDF can also be imported from @pdfme/ui.
+
+const template: Template = {
+  basePdf: BLANK_PDF,
+  schemas: [
+    {
+      a: {
+        type: 'text',
+        position: { x: 0, y: 0 },
+        width: 10,
+        height: 10,
+      },
+      b: {
+        type: 'text',
+        position: { x: 10, y: 10 },
+        width: 10,
+        height: 10,
+      },
+      c: {
+        type: 'text',
+        position: { x: 20, y: 20 },
+        width: 10,
+        height: 10,
+      },
+    },
+  ],
+};
+
+const inputs = [{ a: 'a1', b: 'b1', c: 'c1' }];
 
 function App() {
+  generate({ template, inputs }).then((pdf) => {
+    console.log(pdf);
+
+    // Browser
+    const blob = new Blob([pdf.buffer], { type: 'application/pdf' });
+    window.open(URL.createObjectURL(blob));
+
+    // Node.js
+    // fs.writeFileSync(path.join(__dirname, `test.pdf`), pdf);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
